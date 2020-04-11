bed6d7ddfb16ce6734946722a64ab988e1de11cb
JC-ProPlus <guo121guo@gmail.com>
2020-04-11 16:47:44 +0800

    Bugfix

--- src/Listener/SuspendUserAfterRegistration.php
+++ src/Listener/SuspendUserAfterRegistration.php
@@ -6,32 +6,53 @@ use Carbon\Carbon;
 use Flarum\Extension\ExtensionManager;
 use Flarum\Suspend\Event\Suspended;
 use Flarum\User\Event\Registered;
-use Illuminate\Events\Dispatcher;
+use Flarum\User\User;
+use Illuminate\Contracts\Events\Dispatcher;
 
 class SuspendUserAfterRegistration {
+
     /**
      * @var Registered
      */
-    protected $registeredUser;
+    private $registeredUser;
+
     /**
      * @var Registered
      */
-    protected $registeredActor;
+    private $registeredActor;
+
     /**
      * @param Dispatcher $events
      */
     protected $events;
+
+    /**
+     * @var User $user
+     */
+    private $suspendedUser;
+
+    /**
+     * @var User $actor
+     */
+    private $suspendedActor;
+
     public function handleRegistrationEvent(Registered $event, Dispatcher $Event){
         $this->registeredUser = $event->user;
         $this->registeredActor = $event->actor;
         $this->events = $Event;
+    }
+    public function bootUserModal(User $user, User $actor){
+        $this->registeredUser = $user;
+        $this->registeredActor = $actor;
+        $this->suspendedUser = $user;
+        $this->suspendedActor = $actor;
     }
     public function suspendRegisteredUser() {
         $manager = app(ExtensionManager::class);
 
         if ($manager->isEnabled('flarum-suspend')) {
             $this->registeredUser->suspended_until = Carbon::parse('2038-01-01');
-            $this->events->dispatch(new Suspended($this->registeredUser, $this->registeredActor));
+            app('events')->dispatch(new Suspended($this->suspendedUser, $this->suspendedActor));
         }
     }
 }
