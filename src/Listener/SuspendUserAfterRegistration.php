f0e51eb7273b3f5bc43a4706e892f701c0ca5f4a
JC-ProPlus <guo121guo@gmail.com>
2020-04-11 16:15:33 +0800

    Rewrited listener

--- src/Listener/SuspendUserAfterRegistration.php
+++ src/Listener/SuspendUserAfterRegistration.php
@@ -6,20 +6,23 @@ use Carbon\Carbon;
 use Flarum\Extension\ExtensionManager;
 use Flarum\Suspend\Event\Suspended;
 use Flarum\User\Event\Registered;
+use Illuminate\Events\Dispatcher;
 
 class SuspendUserAfterRegistration {
-    public $registeredUser;
-    public $registeredActor;
-    public function handleRegistrationEvent(Registered $event){
+    protected $registeredUser;
+    protected $registeredActor;
+    protected $events;
+    public function handleRegistrationEvent(Registered $event, Dispatcher $Event){
         $this->registeredUser = $event->user;
         $this->registeredActor = $event->actor;
+        $this->events = $Event;
     }
-    function suspendRegisteredUser() {
+    public function suspendRegisteredUser() {
         $manager = app(ExtensionManager::class);
 
         if ($manager->isEnabled('flarum-suspend')) {
             $this->registeredUser->suspended_until = Carbon::parse('2038-01-01');
-            app('events')->dispatch(new Suspended($this->registeredUser, $this->registeredActor));
+            $this->events->dispatch(new Suspended($this->registeredUser, $this->registeredActor));
         }
     }
 }
