d003cd023586dea543e2f005e6fdc43e159e09ae
JC-ProPlus <guo121guo@gmail.com>
2020-04-13 15:59:10 +0800

    What an idiot I am

--- src/Listener/SuspendUserAfterRegistration.php
+++ src/Listener/SuspendUserAfterRegistration.php
@@ -4,21 +4,24 @@ namespace WvbForum\Butler\Listener;
 
 use Carbon\Carbon;
 use Flarum\Extension\ExtensionManager;
+use Flarum\Suspend\Event\Suspended;
 use Flarum\User\Event\Activated;
+use Flarum\User\User;
 use Illuminate\Contracts\Events\Dispatcher;
 
 class SuspendUserAfterRegistration {
 
     public function subscribe(Dispatcher $event) {
         $event->listen(Activated::class, function (Activated $events) {
-            $this->suspendActivatedUser($events->user);
+            $this->suspendActivatedUser($events->user, $events->user);
         });
     }
 
-    function suspendActivatedUser($user) {
+    function suspendActivatedUser($user, $actor) {
         $manager = app(ExtensionManager::class);
         if ($manager->isEnabled('flarum-suspend')) {
             $user->suspended_until = Carbon::parse('2038-01-01');
+            app('events')->dispatch(new Suspended($user, $actor));
         }
     }
 }
