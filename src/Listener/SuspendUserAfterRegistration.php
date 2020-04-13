a8cee3ceac800105fd94f3114cecfb5dc2e7ba8c
JC-ProPlus <guo121guo@gmail.com>
2020-04-13 11:48:22 +0800

    Suspend method changed

--- src/Listener/SuspendUserAfterRegistration.php
+++ src/Listener/SuspendUserAfterRegistration.php
@@ -5,7 +5,7 @@ namespace WvbForum\Butler\Listener;
 use Carbon\Carbon;
 use Flarum\Extension\ExtensionManager;
 use Flarum\Suspend\Event\Suspended;
-use Flarum\User\Event\Registered;
+use Flarum\User\Event\Activated;
 use Flarum\User\User;
 use Illuminate\Contracts\Events\Dispatcher;
 
@@ -22,31 +22,13 @@ class SuspendUserAfterRegistration {
     public $suspendedActor;
 
     public function subscribe(Dispatcher $event) {
-        $event->listen(Registered::class, function (Registered $events) {
-            //$this->assignSuspendActor($events->user);
-            //$this->handleRegistrationEvent();
-            $this->suspendRegisteredUser($events->user,$events->user);
+        $event->listen(Activated::class, function (Activated $events) {
+            $this->suspendActivatedUser($events->user,$events->user);
         });
     }
-
-    /*function assignSuspendActor(User $user) {
-        if ($user->id == 1) {
-            $this->suspendedActor = $user;
-        }
-        return $this->suspendedActor;
-    }
-
-    function handleRegistrationEvent(Dispatcher $event) {
-        $event->listen(Registered::class, function (Registered $events) {
-            $this->suspendedUser = $events->user;
-            $this->suspendRegisteredUser($this->suspendedUser, $this->suspendedActor);
-        });
-    }*/
-
-    function suspendRegisteredUser($user, $actor) {
+    
+    function suspendActivatedUser($user, $actor) {
         $manager = app(ExtensionManager::class);
-        $this->suspendedUser = $user;
-        $this->suspendedActor = $actor;
         if ($manager->isEnabled('flarum-suspend')) {
             $user->suspended_until = Carbon::parse('2038-01-01');
             app('events')->dispatch(new Suspended($user, $actor));
