b41dd17e81ae82817eb5a3b19c6d64bbe0a46763
JC-ProPlus <guo121guo@gmail.com>
2020-04-13 15:52:27 +0800

    Only revoke user's permission, ignore the notification

--- src/Listener/SuspendUserAfterRegistration.php
+++ src/Listener/SuspendUserAfterRegistration.php
@@ -4,25 +4,21 @@ namespace WvbForum\Butler\Listener;
 
 use Carbon\Carbon;
 use Flarum\Extension\ExtensionManager;
-use Flarum\Suspend\Event\Suspended;
 use Flarum\User\Event\Activated;
-use Flarum\User\User;
 use Illuminate\Contracts\Events\Dispatcher;
 
 class SuspendUserAfterRegistration {
 
     public function subscribe(Dispatcher $event) {
         $event->listen(Activated::class, function (Activated $events) {
-            $admin = new user([$attributes = [$id = 1]]);
-            $this->suspendActivatedUser($events->user, $admin);
+            $this->suspendActivatedUser($events->user);
         });
     }
-    
-    function suspendActivatedUser($user, $actor) {
+
+    function suspendActivatedUser($user) {
         $manager = app(ExtensionManager::class);
         if ($manager->isEnabled('flarum-suspend')) {
             $user->suspended_until = Carbon::parse('2038-01-01');
-            app('events')->dispatch(new Suspended($user, $actor));
         }
     }
 }
